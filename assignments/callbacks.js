bc89c7c7444befac63775874288609dd9584561d
David Francis <davidjayfrancis@gmail.com>
2019-08-10 09:31:45 -0700

    completed problems

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,26 +24,44 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
+// higher order function
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+//function invocation
+getLength(items, (length) => {
+  console.log(`array length  is ${length}`);
+})
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+last(items, (last) => {
+  console.log(last);
+})
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(1,2, (sum) =>{
+  console.log(sum);
+})
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
+multiplyNums(10,69, (prod) => {
+  console.log(prod);
+})
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
@@ -52,4 +70,13 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  let newArray = [];
+  for (let i=0; i<array.length; i++ ) {
+    if (newArray.includes(array[i])) {
+      continue;
+    } else {
+      newArray.push(array[i])
+    }
+  }
+  return cb(newArray);
 }
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,50 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+// Not sure if this is what the problem is looking for, but anyhoo. 
+let ni = 'ni';
+function hao() {
+  console.log(ni + ' hao');
+}
+hao();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
-};
+  // Return a function that when invoked increments and returns a counter variable. 
+
+  // I don't quite get why you need a variable to assign the function to, i.e. why dp you need `newCounter = counter()`
+  let count=0;
+  const inc = function() {
+    count = count +1;
+    return count;
+  }
+  return inc;
+}
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+  let count = 0; 
+  return {
+    increment: function() {
+      count = count + 1;
+    },
+    decrement: function() {
+      count = count - 1;
+    },
+    value: function() {
+      return count;
+    }
+  }
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
 };
+function newFunction() {
+  return 0;
+}
+
